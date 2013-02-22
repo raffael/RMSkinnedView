4beb5a2fadde531f385397f6f6aa831ca19ca7ba
Raffael <raffael@raffael.me>
2013-02-22 15:08:37 +0100

    Updated Docs

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 This subclass of NSView uses a NSImage as pattern or a NSColor to fill its background. The view will be willed using **NSCompositeSourceOver**, so that layers beneath it stay visible when using transparency effects.
 
+![RMSkinnedView Preview](/path/to/img.jpg "Custom NSView subclass with background image")
+
 The core feature of RMSkinnedWindow is the ability to define certain properties directly in the Interface builder. To do this, drag a new ´´´NSView´´´ to your UI and choose ´´´RMSkinnedView´´´ as its class. In the same view of Interface Builder (Identity Inspector), add properties according to the following rules to the **User Defined Runtime Attributes**.
 
 * **backgroundPatternImageName (STRING)** The name of the image in your ressources you want to use as a background pattern for the view. The image can contain transparency effects. The image will be loaded using ´´´[NSImage imageNamed:];´´´. A specified color will be ignored if the pattern image exists.
+
+* **offsetString (STRING)** A string defining a two-dimensional offset for the background pattern drawing. Since the pattern depends on the window's size, you just have to code-and-try until it fits your needs!
 
 * **colorString (STRING)** An RGBa string that defines the color you want to use, if you do not want to make use of pattern images. The format of the RGBa string is ´´´[0-255][0-255][0-255][0.0-1.0]´´´, e.g. ´´´255,128,0,0.5´´´. The color will be ignored if a pattern image has been set.
 
--- RMSkinnedView.h
+++ RMSkinnedView.h
@@ -8,9 +8,9 @@
 // Under BSD License
 
 /**
- * This subclass of NSView uses a NSImage as pattern to fill its background.
+ * This subclass of NSView uses a NSImage as pattern or a NSColor to fill its background.
  *
- * In Interface Builder you can specify the property 'backgroundPatternImageName'
+ * In Interface Builder you can specify the property 'backgroundPatternImageName' and others
  * to specify the image name of the view's background image without subclassing or
  * referencing-and-setting it.
  */
@@ -26,22 +26,22 @@
 
 /** The image to be used as background pattern.
  */
-@property (retain,nonatomic) NSString *backgroundPatternImageName;
+@property (retain,nonatomic) IBOutlet NSString *backgroundPatternImageName;
 
 /** The color, which will be used if image not loaded */
 @property (retain,nonatomic) NSColor *color;
 
 /** The string for the color, which will be used if image not loaded */
-@property (retain,nonatomic) NSString *colorString;
+@property (retain,nonatomic) IBOutlet NSString *colorString;
 
 /** The string for the pattern offset. */
-@property (retain,nonatomic) NSString *offsetString;
+@property (retain,nonatomic) IBOutlet NSString *offsetString;
 @property (assign,nonatomic) NSPoint patternOffset;
 
-@property (retain) NSNumber *cornerRadius;
-@property (assign) BOOL roundedTopLeft;
-@property (assign) BOOL roundedTopRight;
-@property (assign) BOOL roundedBottomLeft;
-@property (assign) BOOL roundedBottomRight;
+@property (retain) IBOutlet NSNumber *cornerRadius;
+@property (assign) IBOutlet BOOL roundedTopLeft;
+@property (assign) IBOutlet BOOL roundedTopRight;
+@property (assign) IBOutlet BOOL roundedBottomLeft;
+@property (assign) IBOutlet BOOL roundedBottomRight;
 
 @end
